88e68619e715df8341bb3bd87531c40bba3f69d1
George C. Privon <accounts5@privon.com>
2019-04-15 00:15:03 -0400

    Brier score and unit test

---
+++ tests/brier_score-test.rkt
@@ -0,0 +1,16 @@
+#lang racket/base
+; brier_score_test.rkt
+; Unit tests for Brier score function
+
+(require rackunit
+         "../code/brier_score.rkt")
+
+; perfect prediction for event occurring
+(check-equal? (brier-score 1 1) 0 "Perfect prediction, event occurs")
+
+; wrong prediction, event occurs
+(check-equal? (brier-score 1 0) 1 "Predicted impossible, event occurs")
+
+; 50/50 scores
+(check-equal? (brier-score 0.5 1) 0.25 "50/50 prediction, event occurs")
+(check-equal? (brier-score 0.5 0) 0.25 "50/50 prediction, event doesn't occur")
