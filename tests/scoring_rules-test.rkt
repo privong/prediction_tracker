5a27e30314ae98c11a9c3ee8fa5fd3ee3108d1b1
George C. Privon <accounts5@privon.com>
2019-04-18 22:45:58 -0400

    update unit tests for scoring rules, add logarithmic score tests

--- tests/scoring_rules-test.rkt
+++ tests/scoring_rules-test.rkt
@@ -3,8 +3,9 @@
 ; Unit tests for Brier score function
 
 (require rackunit
-         "../code/brier_score.rkt")
+         "../code/scoring_rules.rkt")
 
+; Brier Score Checks
 ; perfect prediction for event occurring
 (check-equal? (brier-score 1 1) 0 "Perfect prediction, event occurs")
 
@@ -16,3 +17,7 @@
 ; 50/50 scores
 (check-equal? (brier-score 0.5 1) 0.25 "50/50 prediction, event occurs")
 (check-equal? (brier-score 0.5 0) 0.25 "50/50 prediction, event doesn't occur")
+
+; Logarithmic Score checks
+(check-within (log-score 0.8 1) (log 0.8) 1e-8 "80% forecast, event occurs")
+(check-within (log-score 0.8 0) (log 0.2) 1e-8 "80% forecast, event doesn't occur")
